e527375f5bf59c54088a8fba1dd14b6c6f45c016
Zavir07 <ridwaank10@gmail.com>
2025-06-14 12:10:36 +0600

    Add files via upload

--- class2.py
+++ class2.py
@@ -1,8 +1,38 @@
 # this is the sum code
 
-a = 55
-b = 68
-total = a + b 
-print(f"first number is {a}")
-print(f"second number is {b}")
-print(f"sum is {total}")
+# a = 55
+# b = 68
+# total = a + b 
+# print(f"first number is {a}")
+# print(f"second number is {b}")
+# print(f"sum is {total}")
+
+# a = input("plese enter your first number: ")
+# b = input("plese enter kor tor 2 number: ")
+
+# a = int(a)
+# b = int(b)
+# result = a * b
+# print(f"first number is {a}")
+# print(f"second number is {b}")
+# print(f"multiplication is {result}")
+
+firt_number = 245
+sum = 13454
+nd2_number=sum - firt_number
+
+# mul = 50
+# nd2_number = mul / firt_number
+
+# f_num = sub + _2nd_number
+
+print(f"first number is {firt_number} sum is {sum} what is the 2nd number")
+
+user_answer = int(input("plese insert 2nd number to ans"))
+
+if nd2_number == user_answer:
+    print("correct ans")
+else:
+    print("you lost ni correct ans")    
+    print("give 444 taka for ans")
+
