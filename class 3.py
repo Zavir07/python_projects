ff7e85176302fb0efe9e611448c6e3e933f3a69a
Zavir07 <ridwaank10@gmail.com>
2025-06-15 12:12:46 +0600

    Add files via upload

---
+++ class 3.py
@@ -0,0 +1,86 @@
+firt_number = 2
+mul = 66
+nd2_number = mul / firt_number
+
+
+print(f"first number is {firt_number} multiplicationn is {mul} what is the 2nd number")
+
+user_answer = int(input("plese insert 2nd number to ans"))
+
+if nd2_number == user_answer:
+    print("correct ans")
+else:
+    print("you lost ni correct ans")    
+    print("give 444 taka for ans")
+
+flower = input("aii tor ful chose kor")
+clore1 = input("aii tor color1 chose kor")
+voilet = input("aii tor flower 2 chose kor")
+color2= input("aii tor color2 chose kor")
+misti= input("aii tor misti chose kor")
+pronoun= input("aii tor pronoun chose kor")
+
+
+print(f"{flower} are {clore1}")
+print(f"{voilet} are {color2}")
+print(f"{misti} is sweet and so are {pronoun}")
+
+
+
+print("""
+      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  `'-'
+    '---''(_/--'  `-'\_)   
+
+""")
+
+
+age = 18
+
+if age > 18:
+    print("you are avode 18")
+elif age < 18:
+    print("tumi 18 er niche bari jao")
+else:
+    print("tumi 18 er soman")
+
+car_speed = 0
+
+while True:
+    gaddi = input("press some r to start the lambogini: ")
+
+    if gaddi =="r":
+        print("lambo started brum bruuuuuuuum ")
+
+        while True:
+            garir_input = input("w/a/s/d press koro: ")
+
+            if garir_input == "w":
+                car_speed = car_speed + 1
+                print(f"car is moving straight, speed: {car_speed}")
+                
+
+            if garir_input == "s":
+                print("car is moving back")
+
+            if garir_input == "a":
+                print("car is moving left")
+
+            if garir_input == "d":
+                print("car is moving right")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
